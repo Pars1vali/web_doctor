d07e1b9e331bb1a1a879a9cb54995ed19678c0cb
Chumak Vladimir <parsival427@gmail.com>
2024-02-06 16:52:33 +0300

    06.02.2024 16:51 +change structure code +add info in ui

--- main.py
+++ main.py
@@ -18,6 +18,41 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
+def createPassword():
+    password = st.text_input(label=ui["user"]["password"], type="password")
+    password_chesk = st.text_input(label=ui["user"]["password_chesk"], type="password")
+    if(password==password_chesk):
+        return password
+    else:
+        st.error(ui["error"]["passwords_mismatch"])
+
+def controller():
+
+    if st.session_state['mode'] == 'login':
+        login = st.text_input(label=ui["login"])
+        password = st.text_input(label=ui["password"], type="password", key="login_password")
+        login_btn = st.button(label=ui["button"]["login_account_btn"], type="primary")
+        if (login_btn):
+            st.write(net.Client.authentication(login, password))
+    elif st.session_state['mode'] == 'create':
+        firstname = st.text_input(label=ui["user"]["firstname"])
+        lastname = st.text_input(label=ui["user"]["lastname"])
+        surname = st.text_input(label=ui["user"]["surname"])
+        post = st.text_input(label=ui["user"]["post"])
+        organization = st.text_input(label=ui["user"]["organization"])
+        phone_number = st.text_input(label=ui["user"]["phone_number"])
+        username = st.text_input(label=ui["user"]["username"])
+        email = st.text_input(label=ui["user"]["email"])
+        password = createPassword()
+
+        all_fields = all([firstname, lastname, surname, post, organization, phone_number, username, email, password])
+        crete_account_btn = st.button(label=ui["button"]["crete_account_btn"], type="primary")
+        if crete_account_btn:
+            if all_fields:
+                new_user = user.Doctor(firstname, lastname, surname, post, organization, phone_number, username, email, password)
+            else:
+                st.error(ui["error"]["fields_incomplete"])
+
 def init():
     st.image(image=ui_images["main_icno"], width=200)
     if 'mode' not in st.session_state:
@@ -35,46 +70,9 @@ def init():
     if login_link_btn:
         st.session_state['mode'] = 'login'
 
-def createPassword():
-    password = st.text_input(label=ui["user"]["password"], type="password")
-    password_chesk = st.text_input(label=ui["user"]["password_chesk"], type="password")
-    if(password==password_chesk):
-        return password
-    else:
-        st.error("Пароли не совпадают")
-
-def controller():
-
-    if st.session_state['mode'] == 'login':
-        login = st.text_input(label="Логин")
-        password = st.text_input(label="Пароль", type="password",)
-        login_btn = st.button(label="Войти", type="primary")
-        if (login_btn):
-            st.write(net.Client.authentication(login, password))
-    elif st.session_state['mode'] == 'create':
-        firstname = st.text_input(label=ui["user"]["firstname"])
-        lastname = st.text_input(label=ui["user"]["lastname"])
-        surname = st.text_input(label=ui["user"]["surname"])
-        post = st.text_input(label=ui["user"]["post"])
-        organization = st.text_input(label=ui["user"]["organization"])
-        phone_number = st.text_input(label=ui["user"]["phone_number"])
-        username = st.text_input(label=ui["user"]["username"])
-        email = st.text_input(label=ui["user"]["email"])
-        password = createPassword()
-
-        all_property = all([firstname, lastname, surname, post, organization, phone_number, username, email, password])
-        crete_account_btn = st.button(label=ui["button"]["crete_account_btn"], type="primary")
-        if crete_account_btn:
-            if all_property:
-                new_user = user.Doctor(firstname, lastname, surname, post, organization, phone_number, username, email, password)
-            else:
-                st.error("Не все поля заполнены")
-
-
-
 if __name__ == '__main__':
     user_language = "ru"
-    local_css("resources/style.css")
+    local_css("resources/style/style.css")
     load_localization(f"resources/ui/localization/localization_{user_language}.json")
     load_images("resources/ui/image.json")
     init()
