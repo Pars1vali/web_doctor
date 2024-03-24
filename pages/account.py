da3fbd2e7cbf0174465da9b4e4ffe30222f9ac36
Chumak Vladimir <parsival427@gmail.com>
2024-03-24 19:59:48 +0300

    design

--- pages/account.py
+++ pages/account.py
@@ -25,13 +25,15 @@ def load_resourses():
 
 def foother():
     st.divider()
-    clients, exit, remove_account = st.columns(3)
-    if exit.button(label="Выход", type="primary"):
-        st.switch_page("home.py")
-    if remove_account.button("Удалить аккаунт", type="primary"):
-        pass
-    if clients.button("Клиенты", type="primary"):
+    clients, settings = st.columns(2)
+    # if remove_account.button("Удалить аккаунт", type="primary", use_container_width=True):
+    #     pass
+    if clients.button("Клиенты", use_container_width=True):
         st.switch_page("pages/clients.py")
+    if settings.button("Настройки", type="primary", use_container_width=True):
+        st.switch_page("pages/settings.py")
+    # if exit.button(label="Выход", type="primary", use_container_width=True):
+    #     st.switch_page("home.py")
 
 def controller():
     foother()
@@ -40,6 +42,8 @@ def init():
     # st.header("Личный кабинет специалиста")
     data_doctor = json.loads(st.session_state.get("data_doctor"))
     st.title(f"{data_doctor[1]} {data_doctor[2]} {data_doctor[3]}")
+    image_container = st.container(border=True)
+    image_container.image("experiment/doctor.png", use_column_width="auto")
     annotated_text((f"{data_doctor[5]}", "Организация", "#afa"))
     annotated_text((f"{data_doctor[4]}", "Должность", "#afa"))
     annotated_text((f"{data_doctor[8]}", "Почта", "#afa"))
--- pages/clients.py
+++ pages/clients.py
@@ -9,12 +9,69 @@ ui, ui_images = None, None
 clients = net.Doctor.get_client(json.loads(st.session_state.get("data_doctor"))[0])
 
 
+def load_resourses(file_style,file_localization, file_images ):
+    global ui, ui_images
+    loader.set_resources(file_style, file_localization, file_images)
+
+    st.markdown(loader.load_styles(),unsafe_allow_html=True)
+    ui, ui_images = loader.load_localization(), loader.load_images()
+
+
+def init():
+    # data_doctor = json.loads(st.session_state.get("data_doctor"))
+    # st.header(f"{data_doctor[1]} {data_doctor[2]} {data_doctor[3]}")
+    controller()
+
+
+def controller():
+    show_clients()
+    st.divider()
+    foother()
+
+
+def search_client(search_query):
+    st.markdown("Найдено:")
+    is_found = False
+    clients_data = json.loads(clients)
+    for client in clients_data:
+        first_and_lastname = f"{client[2]} {client[1]}"
+        if first_and_lastname == search_query or search_query==client[2] or search_query==client[1]:
+            is_found = True
+            with st.expander(f"{client[2]} {client[1]} {client[3]}"):
+                annotated_text((f"{client[5]}", "Почта", "#afa"))
+                annotated_text((f"{client[6]}", "Номер телефона", "#afa"))
+                if st.button(label="Данные о здоровье", type="primary", key=f"{client[0]}_search"):
+                    show_data(client[5])
+
+    if is_found is False:
+        st.info("Не найдено")
+
+
+def show_clients():
+    st.header("Клиенты")
+    search = st.text_input(label="Найти", placeholder="Иванов Иван")
+    if search is not None and search != "":
+        search_client(search)
+
+    if clients == "null":
+        st.info("Нет клиентов")
+        return
+    st.markdown("Все клиенты")
+
+    clients_data = json.loads(clients)
+    for client in clients_data:
+        with st.expander(f"{client[2]} {client[1]} {client[3]}"):
+            annotated_text((f"{client[5]}", "Почта", "#afa"))
+            annotated_text((f"{client[6]}", "Номер телефона", "#afa"))
+            # annotated_text((f"{client[4]}", "Врач", "#afa"))
+            if st.button(label="Данные о здоровье", type="primary", key=f"{client[0]}"):
+                # data_input = st.popover("vfvf")
+                show_data(client[5])
+
 def show_points(points):
     for num, point in enumerate(points):
         point_name, point_value, point_metric = fit_unit.FitUnit.get_data_unit(point, points[point])
         st.metric(f"{point_name}", f"{point_value}", point_metric)
-
-
 
 def show_data(email):
     response = json.loads(net.Doctor.get_client_data(email))
@@ -29,52 +86,19 @@ def show_data(email):
                 st.subheader("Данных за это время не собрано")
 
 
-def load_resourses(file_style,file_localization, file_images ):
-    global ui, ui_images
-    loader.set_resources(file_style, file_localization, file_images)
 
-    st.markdown(loader.load_styles(),unsafe_allow_html=True)
-    ui, ui_images = loader.load_localization(), loader.load_images()
-
-
-def init():
-    data_doctor = json.loads(st.session_state.get("data_doctor"))
-    # st.header(f"{data_doctor[1]} {data_doctor[2]} {data_doctor[3]}")
-    controller()
-
-def controller():
-    header()
-    st.divider()
-    show_clients()
-
-
-def show_clients():
-    st.subheader("Пациенты")
-    if clients == "null":
-        st.info("Нет клиентов")
-        return
-
-    clients_data = json.loads(clients)
-    for client in clients_data:
-        with st.expander(f"{client[2]} {client[1]} {client[3]}"):
-            annotated_text((f"{client[5]}", "Почта", "#afa"))
-            annotated_text((f"{client[6]}", "Номер телефона", "#afa"))
-            # annotated_text((f"{client[4]}", "Врач", "#afa"))
-            if st.button(label="Данные о здоровье", type="primary", key=f"{client[0]}"):
-                # data_input = st.popover("vfvf")
-                show_data(client[5])
-
-def header():
+def foother():
     account, exit = st.columns(2)
-    if account.button(label="Аккаунт", type="primary"):
+    if account.button(label="Аккаунт", use_container_width=True):
         st.switch_page("pages/account.py")
-    if exit.button(label="Выйти", type="primary"):
+    if exit.button(label="Выйти", type="primary", use_container_width=True):
         st.switch_page("home.py")
 
 
 
+
 if __name__ == '__main__':
-    user_language = "ru"
+    user_language = loader.language
     load_resourses(
         file_style="resources/style/style.css",
         file_localization=f"resources/ui/localization/localization_{user_language}.json",
