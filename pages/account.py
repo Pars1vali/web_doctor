c44f785e1d5a9f0bd0722fb312e6830c2d8fb659
Chumak Vladimir <parsival427@gmail.com>
2024-03-28 20:47:19 +0300

    add photo profile

--- pages/account.py
+++ pages/account.py
@@ -1,13 +1,8 @@
 from core import loader
 from annotated_text import annotated_text
-import streamlit as st, json
-
-# from st_pages import Page, show_pages, hide_pages
-#
-# show_pages([
-#     Page("pages/account.py", "Личный кабинет"),
-#     Page("pages/clients.py", "Клиенты")
-# ])
+from PIL import Image
+from io import BytesIO
+import streamlit as st, json, base64, io
 
 ui, ui_images = None, None
 
@@ -23,32 +18,33 @@ def load_resourses():
     st.markdown(loader.load_styles(), unsafe_allow_html=True)
     ui, ui_images = loader.load_localization(), loader.load_images()
 
-def foother():
-    st.divider()
-    clients, settings = st.columns(2)
-    # if remove_account.button("Удалить аккаунт", type="primary", use_container_width=True):
-    #     pass
-    if clients.button("Клиенты", use_container_width=True):
-        st.switch_page("pages/clients.py")
-    if settings.button("Настройки", type="primary", use_container_width=True):
-        st.switch_page("pages/settings.py")
-    # if exit.button(label="Выход", type="primary", use_container_width=True):
-    #     st.switch_page("home.py")
-
 def controller():
+    st.divider()
     foother()
 
 def init():
     # st.header("Личный кабинет специалиста")
     data_doctor = json.loads(st.session_state.get("data_doctor"))
     st.title(f"{data_doctor[1]} {data_doctor[2]} {data_doctor[3]}")
-    image_container = st.container(border=True)
-    image_container.image("experiment/doctor.png", use_column_width="auto")
+    show_photo(data_doctor[12])
     annotated_text((f"{data_doctor[5]}", "Организация", "#afa"))
     annotated_text((f"{data_doctor[4]}", "Должность", "#afa"))
     annotated_text((f"{data_doctor[8]}", "Почта", "#afa"))
     annotated_text((f"{data_doctor[6]}", "Номер телефона", "#afa"))
     controller()
+
+def show_photo(image):
+    image_container = st.container(border=True)
+    image_str = image[2:-1]
+    image_data = base64.b64decode(image_str)
+    image = Image.open(io.BytesIO(image_data))
+    image_container.image(image, use_column_width="Auto")
+def foother():
+    clients, settings = st.columns(2)
+    if clients.button("Клиенты", use_container_width=True):
+        st.switch_page("pages/clients.py")
+    if settings.button("Настройки", type="primary", use_container_width=True):
+        st.switch_page("pages/settings.py")
 
 if __name__=='__main__':
     load_resourses()
